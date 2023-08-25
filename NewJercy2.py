d856d4fa4dd2c6d2590fa6cbd68ef6716ced92f4
Datchthana1 <140144536+Datchthana1@users.noreply.github.com>
2023-08-25 11:48:16 +0700

    enter

--- NewJercy2.py
+++ NewJercy2.py
@@ -3,20 +3,20 @@ import Ohio as O
 O.Name.Input()
 Loop = True
 while Loop == True:
-    choose = str(input("คุณต้องการเลือก ห.ร.ม หรือ ค.ร.น\nกด 1 เพื่อคำนวณค่าตัวหารร่วมมากของจำนวนเต็มดังกล่าว\nกด 2 เพื่อคำนวณค่าตัวคูณร่วมน้อยของจำนวนเต็มดังกล่าว\nคุณต้องการคำนวณสิ่งไหน : "))
-    Number1 = int(input("ตัวเลขตัวที่ 1 : "))
-    Number2 = int(input("ตัวเลขตัวที่ 2 : "))
+    choose = str(input("GCD or LCM\nกด 1:GCD\nกด 2:LCM\nWhich do you want to do?\n : "))
+    Number1 = int(input("Number1 : "))
+    Number2 = int(input("Number2 : "))
     if choose == "1":
-        print("คุณเลือก ห.ร.ม")
+        print("You choose GCD")
         if Number1 > 0 and Number2 > 0:
-            print("ห.ร.ม คือ {}".format(m.gcd(Number1,Number2)))
+            print("GCD is {}".format(m.gcd(Number1,Number2)))
     elif choose == "2":
-        print("คุณเลือก ค.ร.น")
+        print("You choose LCM")
         if Number1 > 0 and Number2 > 0:
-            print("ค.ร.น คือ {}".format(m.lcm(Number1,Number2)))
+            print("LCM is {}".format(m.lcm(Number1,Number2)))
     else:
-        print("ไม่สามารถระบุความต้องการได้")
-    ways = str(input("คุณต้องการคำนวณอีกครั้งหรือไม่ 1 = ต้องการ 2 = ไม่ต้องการ : "))
+        print("Cannot define??!")
+    ways = str(input("Do you want to repeat? 1 = Yes 2 = No : "))
     if ways == "1":
         Loop = True
     else:
