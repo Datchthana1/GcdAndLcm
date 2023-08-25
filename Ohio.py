a353db21f5dd6e23e7546070500da7c5bb1c6377
Datchthana1 <140144536+Datchthana1@users.noreply.github.com>
2023-08-25 11:52:43 +0700

    Update Ohio.py

--- Ohio.py
+++ Ohio.py
@@ -6,8 +6,19 @@ class Name:
     def GetFnandLn(self):
         return self.FirstName + " " + self.LastName + " " + self.SID
     def Input():
-        Fname = str(input("โปรดใส่ชื่อของคุณ : "))
-        Lname = str(input("โปรดใส่นามสกุลของคุณ : "))
-        StudentID = str(input("โปรดใส่รหัสประจำตัว : "))
+        Fname = str(input("Please Type your First Name : "))
+        Lname = str(input("Please Type your Last name  : "))
+        StudentID = str(input("Please Type Your Student ID : "))
         P1 = Name(Fname,Lname,StudentID)
         print(P1.GetFnandLn())
+class University:
+    def __str__(self,UniversityName,Department):
+        self.UniversityName = UniversityName
+        self.Department = Department
+    def Output(self):
+        return self.UniversityName + "and in Department : " + self.Department
+    def Input():
+        UniversityName = str(input("What is your University Name? : "))
+        Department = str(input("What is your department? : "))
+        U1 = University(UniversityName,Department)
+        print(U1)
